45e64a72b5b5317efa99d23ce9c4f7fb5dacce41
Kabi16 <86357347+Kabi16@users.noreply.github.com>
2022-07-20 19:47:17 +0530

    Update publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -5,8 +5,8 @@
 @FOR %%A IN (%*) DO @IF "%%A"=="nochain" @SET CHAINEDCALL=false
 @FOR %%A IN (%*) DO @IF "%%A"=="Release" @SET MODE=Release
 
-@SET SOLU="C:\Project\HelloWorld\src\dotnetHelloWorld\dotnetHelloWorld.csproj"
-@SET PROFILE="C:\Project\HelloWorld\src\dotnetHelloWorld\bin\%MODE%\staging"
+@SET SOLU="C:\dotnethelloworld\src\dotnetHelloWorld\dotnetHelloWorld.csproj"
+@SET PROFILE="C:\dotnethelloworld\src\dotnetHelloWorld\bin\%MODE%\staging"
 
   
 @SET MSBUILDDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
